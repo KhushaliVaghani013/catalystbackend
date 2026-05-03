354c4858e62383fe2c02df69a55581e572dc0248
KhushaliVaghani013 <vaghanikhushali013@gmail.com>
2026-05-03 17:54:28 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # from websockets import connect
 import pyodbc
 
+
+# DATABASE_URL = "sqlite:///./test.db"
 DATABASE_URL = "mssql+pyodbc://@KHUSHI\\SQLEXPRESS01/Catalyst?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
 
 engine = create_engine(DATABASE_URL)
@@ -15,4 +17,4 @@ print("Data base connect successfully!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
 
 
-# DATABASE_URL = "sqlite:///./test.db"
+
