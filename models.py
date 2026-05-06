50c7631f16c040998f0f873a98ceb41af25be1e2
KhushaliVaghani013 <vaghanikhushali013@gmail.com>
2026-05-06 16:45:33 +0530

    Update models.py

--- models.py
+++ models.py
@@ -5,11 +5,11 @@ class GetQuote(Base):
     __tablename__ = "getquote"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    email = Column(String, nullable=False)
-    country_code = Column(String, nullable=True)
-    phone = Column(String, unique=True, nullable=True)
-    service = Column(String, nullable=False)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    country_code = Column(String(50), nullable=False)
+    phone = Column(String(50), unique=True, nullable=True)
+    service = Column(String(255), nullable=False)
     quantity = Column(Integer, nullable=False)
-    material = Column(String, nullable=False)
-    file_upload = Column(String, nullable=True)  # file path
+    material = Column(String(255), nullable=False)
+    file_upload = Column(String(255), nullable=True)  # file path
