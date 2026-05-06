6dc7a93006cf74558ae32c13e09c878a4268d375
KhushaliVaghani013 <vaghanikhushali013@gmail.com>
2026-05-06 16:44:10 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,17 +4,21 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import pyodbc
 
 
-# DATABASE_URL = "sqlite:///./test.db"
-DATABASE_URL = "mssql+pyodbc://@KHUSHI\\SQLEXPRESS01/Catalyst?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
 
-engine = create_engine(DATABASE_URL)
+# hostinger
+DATABASE_URL = "mysql+pymysql://u778110718_Khushi:Cattech9374@auth-db1951.hstgr.io:3306/u778110718_catalyst"
+
+
+engine = create_engine(DATABASE_URL,pool_pre_ping=True,        # Tests connection before using it ✅
+    pool_recycle=280,          # Recycle connections before MySQL times them out
+    pool_size=5,
+    max_overflow=10,
+    connect_args={
+        "connect_timeout": 30
+    })
 
 SessionLocal = sessionmaker(bind=engine)
 print("Data base connect successfully")
 
 Base = declarative_base()
 print("Data base connect successfully!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-
-
-
-
