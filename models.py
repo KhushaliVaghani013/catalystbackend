9925982a06bc7e1fbb806ce7ef7611a7887420e7
KhushaliVaghani013 <vaghanikhushali013@gmail.com>
2026-05-07 11:43:21 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,9 +6,9 @@ class GetQuote(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), nullable=False)
-    email = Column(String(100), unique=True, nullable=False)
-    country_code = Column(String(50), nullable=False)
-    phone = Column(String(50), unique=True, nullable=True)
+    email = Column(String(100), nullable=False)
+    countryCode = Column(String(50), nullable=False)
+    phone = Column(String(50), nullable=True)
     service = Column(String(255), nullable=False)
     quantity = Column(Integer, nullable=False)
     material = Column(String(255), nullable=False)
