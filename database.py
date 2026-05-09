9b4e3ff37b7e11760041841072fcb94fbc57dc33
KhushaliVaghani013 <vaghanikhushali013@gmail.com>
2026-05-09 18:05:28 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,23 +1,45 @@
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 # from websockets import connect
 import pyodbc
+import os
 
+
+# Load .env file
+load_dotenv()
+
+# Get environment variables
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_PORT = os.getenv("DB_PORT")
 
 
 # hostinger
-DATABASE_URL = "mysql+pymysql://u778110718_Khushi:Cattech9374@auth-db1951.hstgr.io:3306/u778110718_catalyst"
+DATABASE_URL = (f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
+f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 
-engine = create_engine(DATABASE_URL,pool_pre_ping=True,        # Tests connection before using it ✅
-    pool_recycle=280,          # Recycle connections before MySQL times them out
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=280,
     pool_size=5,
     max_overflow=10,
     connect_args={
         "connect_timeout": 30
-    })
+    }
+)
 
-SessionLocal = sessionmaker(bind=engine)
+# Session
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 print("Data base connect successfully")
 
 Base = declarative_base()
